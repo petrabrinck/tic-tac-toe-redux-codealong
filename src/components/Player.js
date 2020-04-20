ddda780af1e04640b791b6e44da918175484e918
Petra B <petrabrinck@icloud.com>
2020-04-20 12:01:59 +0200

    adjusted css

--- src/components/Player.js
+++ src/components/Player.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 export const Player = ({ value }) => (
   <>
-    {value === 'x' && <span role="img" aria-label="x">❌</span>}
-    {value === 'o' && <span role="img" aria-label="o">⭕️</span>}
+    {value === 'x' && <span role="img" aria-label="x">🦋</span>}
+    {value === 'o' && <span role="img" aria-label="o">🦄</span>}
+    {/* {value === 'x' && <span role="img" aria-label="x">♡</span>}
+    {value === 'o' && <span role="img" aria-label="o">♥️</span>} */}
+    {/* {value === 'x' && <span role="img" aria-label="x">☀︎</span>}
+    {value === 'o' && <span role="img" aria-label="o">☂︎</span>} */}
+    {/* {value === 'x' && <span role="img" aria-label="x">❌</span>}
+    {value === 'o' && <span role="img" aria-label="o">⭕️</span>} */}
   </>
 )
